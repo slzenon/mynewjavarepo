1b4180ac33d18506a735bf0f44c8ab696ef6819b
gishub <slzenon23@gmail.com>
2024-03-07 17:41:27 -0600

    java function

---
+++ jclass.js
@@ -0,0 +1,16 @@
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age(x) {
+    return x - this.year;
+  }
+}
+
+const date = new Date();
+let year = date.getFullYear();
+
+const myCar = new Car("Ford", 2014);
+document.getElementById("demo").innerHTML=
+"My car is " + myCar.age(year) + " years old.";
